a6ba9998ba3f0e9f89b2225599ab9b8f63407310
ivantelizhenko <telizenkoo@gmail.com>
2025-01-22 14:59:13 +0200

    add message Empty for users.length===0

--- src/features/users/userTable/UsersLists.tsx
+++ src/features/users/userTable/UsersLists.tsx
@@ -1,13 +1,29 @@
 import styled from 'styled-components';
 import UserListItem from './UserListItem';
 import { useAppState } from '../../../contexts/userContext/AppContext';
+import Message from '../../../ui/Message';
 
 const StyledUsersLists = styled.ul`
   padding: 2.8rem 3.6rem;
 `;
+const MessaageContainer = styled.div`
+  padding: 2.8rem 3.6rem;
+  display: flex;
+  justify-content: center;
+  p {
+    font-size: 2.4rem;
+  }
+`;
 
 function UsersLists() {
   const { users } = useAppState();
+
+  if (users.length === 0)
+    return (
+      <MessaageContainer>
+        <Message>Oops! You should add user. 😉</Message>
+      </MessaageContainer>
+    );
 
   return (
     <StyledUsersLists>
