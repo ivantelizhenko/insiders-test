85683a31a6a0301f0b54cfae4981048959c9e007
ivantelizhenko <telizenkoo@gmail.com>
2025-01-25 20:48:36 +0200

    a little changes in useToggleFilterLink hook

--- src/hooks/useToggleFilterLink.ts
+++ src/hooks/useToggleFilterLink.ts
@@ -10,19 +10,14 @@ export function useToggleFilterLink() {
 
     const comparisonIndex = valueSetArray.findIndex(el => el === value);
 
-    if (valueSetArray.length === 0) {
-      valueSetArray = [value];
-      console.log('i am the first filter');
-    } else if (valueSetArray.length > 0 && comparisonIndex === -1) {
+    if (valueSetArray.length === 0) valueSetArray = [value];
+    else if (valueSetArray.length > 0 && comparisonIndex === -1)
       valueSetArray = [...valueSetArray, value];
-      console.log('i am the new filter');
-    } else if (valueSetArray.length > 0 && comparisonIndex !== -1) {
+    else if (valueSetArray.length > 0 && comparisonIndex > -1) {
       valueSetArray = valueSetArray.filter(el => el !== value);
-      console.log('i shoud be the deleted filter');
     }
 
     searchParams.set(name, valueSetArray.join(','));
-    if (!valueSetArray.at(0)) searchParams.delete(name);
     if (!valueSetArray.at(0)) searchParams.delete(name);
     setSeachParams(searchParams);
   }
